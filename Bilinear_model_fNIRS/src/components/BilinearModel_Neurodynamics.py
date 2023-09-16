19eef1d40254d3a11c09425820a50d845a4352d4
MarSHub <madlsh@icloud.com>
2023-09-16 15:33:42 -0600

    [Feature]Hemodynamics-draft

--- Bilinear_model_fNIRS/src/components/BilinearModel_Neurodynamics.py
+++ Bilinear_model_fNIRS/src/components/BilinearModel_Neurodynamics.py
@@ -1,21 +1,6 @@
 import numpy as np
 
 
-def Neurodynamics_Jt(A, B, U):
-    nRegions = A.shape[0]
-
-    for t in range(U.shape[1]):
-        T = np.zeros(nRegions)
-        for uu in range(U.shape[0]):
-            tmp = U[uu, t] * B[:, :, uu]
-            T = -0.5 * np.exp(T + tmp)
-
-    SI = np.diag(A)
-    new_diag = np.exp(SI) / 2 + SI
-    A -= np.diagflat(new_diag)
-    return A + T
-
-
 def Neurodynamics_Z(Z, t, A, B, C, U):
     nRegions = A.shape[0]
     index = min(int(t * 10), U.shape[1] - 1)
